140702898ce985c2644957f4e094c0bf050c9d17
hankballew <30753460+hankballew@users.noreply.github.com>
2025-04-09 21:28:33 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * config.php
+ * Database credentials & basic site configuration.
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'otf_calendar');
+define('DB_USER', 'otf_calendar');
+define('DB_PASS', 'otf_calendar');
+
+// Optionally define other global settings
+// define('SITE_URL', 'http://yourserver/otf_calendar/public'); // if needed
+?>
